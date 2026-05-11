5580dca6f9fb36bbc6ff42e6fb87ca7e267d1df1
Andrew Siwko <asiwko@siwko.org>
2026-05-11 11:04:21 -0400

    let's build a provider only tf set to extract schema

---
+++ 01-variables.tf
@@ -0,0 +1,5 @@
+variable "LINODE_API_KEY" {
+  description = "The key to the Linode API"
+  type        = string
+  sensitive   = true
+}
---
+++ 02-providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+    }
+    google = {
+      source  = "hashicorp/google"
+    }
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+    }
+    linode = {
+      source = "linode/linode"
+    }
+    oci = {
+      source  = "oracle/oci"
+    }
+    time = {
+      source = "hashicorp/time"
+    }
+  }
+  backend "local" {
+    path = "/container_shared/tfstate/schema-explorer.tfstate"
+  }
+
+provider "linode" {
+  token = var.LINODE_API_KEY
+}
+
