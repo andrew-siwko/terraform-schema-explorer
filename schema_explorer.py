a30a435dcc5958182267c80881e4aa033b13eaf3
Andrew Siwko <asiwko@siwko.org>
2026-05-13 19:43:33 -0400

    let's explore my provider as I add to the go code...

--- schema_explorer.py
+++ schema_explorer.py
@@ -46,6 +46,9 @@ def scan_for_keys(data, target_keys):
 
 
 ps=schema['provider_schemas']
+print('========================== mirror =========================')
+print(json.dumps(ps['registry.terraform.io/andrew/property-mirror'], indent=2))
+print('========================== mirror =========================')
 print('ps',json.dumps(limit_depth(ps,2),indent=2))
 fv=schema['format_version']
 # don't bother to print this, it's always 1.0
