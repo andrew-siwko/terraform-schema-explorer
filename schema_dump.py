08016c39e6040110861474f3628b1a266caf87cc
Andrew Siwko <asiwko@siwko.org>
2026-05-13 21:17:22 -0400

    strip out some keys to save space

--- schema_dump.py
+++ schema_dump.py
@@ -1,4 +1,4 @@
 import json
 import sys  
-
-print(json.dumps(json.load(sys.stdin),indent=2))
+from schema_explorer import limit_depth, trim_keys, scan_for_keys
+print(json.dumps(trim_keys(json.load(sys.stdin),['version','description','description_kind']),indent=2))
